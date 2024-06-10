c073195756063032407915b6a9f4d1436e675264
John Doe <s.hussain_10@hotmail.com>
2024-06-10 21:21:55 +0300

    t

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,6 +13,6 @@
     </x-slot>
 
     <div class="p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
-        {{ __("You're logged in!")  }}
+        {{ __("test. You're logged in!")  }}
     </div>
 </x-app-layout>
